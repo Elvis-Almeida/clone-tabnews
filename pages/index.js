81809fc5145334719d13b9e78b81084c22f0d22a
Elvis Almeida <70353348+Elvis-Almeida@users.noreply.github.com>
2023-07-06 00:22:24 +0000

    mensagem alterada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Você é fera demais. Arrocha!</h1>
+    return <h1>Soldado promovido!! Desejo tudo de bom nesse novo modo de jogo co-op Multplayer kk</h1>
 }
 
 export default Home;
