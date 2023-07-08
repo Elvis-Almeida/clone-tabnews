e5bc0376588fd4cce5be423caafbd937de7887af
Elvis Almeida <70353348+Elvis-Almeida@users.noreply.github.com>
2023-07-08 21:14:45 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Soldado promovido!! Desejo tudo de bom nesse novo modo de jogo co-op Multplayer kk</h1>
+  return (
+    <h1>
+      Soldado promovido!! Desejo tudo de bom nesse novo modo de jogo co-op
+      Multplayer kk
+    </h1>
+  );
+}
+
+function teste(params) {
+  console.log("sdasdas");
 }
 
 export default Home;
